4f7cfcd93f13ddfb59ef253f89e75f129cf5e357
dwong <dwong@acemetrix.com>
2013-02-26 01:56:53 -0800

    building only jpegtran, setting the output bin directory

--- build.js
+++ build.js
@@ -13,10 +13,9 @@ which ('make', function (err) {
     if (process.platform === 'darwin' || process.platform === 'linux') {
         var binDir = path.dirname(binPath);
         var host = (process.platform === 'darwin') ? 'i686-apple-darwin' : 'i686-pc-linux-gnu';
-        var buildScript = 'make clean &&' +
-                          './configure --disable-shared --host ' + host + ' CFLAGS=\'-O3 -m32\' LDFLAGS=-m32 &&' +
-                          'make &&' +
-                          'mv jpegtran ' + binDir;
+        var buildScript = 'make clean && ' +
+                          './configure --disable-shared --host ' + host + ' CFLAGS=\'-O3 -m32\' LDFLAGS=-m32 && ' +
+                          'make install prefix=' + __dirname + '/libjpeg-turbo/ bindir=' + binDir + ' bin_PROGRAMS=jpegtran'
         exec(buildScript, { cwd: './libjpeg-turbo/' }, function (err) {
             if (err) {
                 return console.log(err.red);
