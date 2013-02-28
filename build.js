4cec1dfec46b84070c119eabd91050a8c432a410
dwong <dwong@acemetrix.com>
2013-02-28 02:36:21 -0800

    modified paths to diverge on 32 and 64 bit linux architectures

--- build.js
+++ build.js
@@ -12,9 +12,8 @@ which ('make', function (err) {
 
     if (process.platform === 'darwin' || process.platform === 'linux') {
         var binDir = path.dirname(binPath);
-        var host = (process.platform === 'darwin') ? 'i686-apple-darwin' : 'i686-pc-linux-gnu';
-        var buildScript = 'make clean && ' +
-                          './configure --disable-shared --host ' + host + ' CFLAGS=\'-O3 -m32\' LDFLAGS=-m32 && ' +
+        var host = (process.platform === 'darwin') ? ' --host i686-apple-darwin' : '';
+        var buildScript = './configure --disable-shared ' + host + ' CFLAGS=\'-O3 -m64\' LDFLAGS=-m64 && ' +
                           'make install prefix=' + __dirname + '/libjpeg-turbo/ bindir=' + binDir + ' bin_PROGRAMS=jpegtran'
         exec(buildScript, { cwd: './libjpeg-turbo/' }, function (err) {
             if (err) {
