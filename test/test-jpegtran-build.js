458df3ceb2b0dd506e064be9d8117fbf5939abea
Sindre Sorhus <sindresorhus@gmail.com>
2013-03-03 19:21:36 +0100

    Code style fixes

--- test/test-jpegtran-build.js
+++ test/test-jpegtran-build.js
@@ -1,9 +1,9 @@
 /*global describe, it */
 'use strict';
+var fs = require('fs');
 var assert = require('assert');
 var exec = require('child_process').exec;
 var binPath = require('../lib/jpegtran-bin.js').path;
-var fs = require('fs');
 
 describe('JPEGTran rebuild', function () {
     it('it should rebuild the jpegtran binaries', function (cb) {
