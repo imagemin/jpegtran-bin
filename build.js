1a9ebaf9df8af0e48d194b59f48e6fe1ae705e91
Sindre Sorhus <sindresorhus@gmail.com>
2013-04-08 18:24:00 +0200

    Convert to tabs

--- build.js
+++ build.js
@@ -6,37 +6,37 @@ var which = require('which');
 var binPath = require('./lib/jpegtran-bin.js').path;
 
 module.exports = function () {
-    if (process.platform === 'darwin' || process.platform === 'linux') {
-        which('make', function (err) {
-            if (err) {
-                return console.log(err);
-            }
+	if (process.platform === 'darwin' || process.platform === 'linux') {
+		which('make', function (err) {
+			if (err) {
+				return console.log(err);
+			}
 
-            var binDir = path.dirname(binPath);
-            var configureFlags = '--disable-shared ';
+			var binDir = path.dirname(binPath);
+			var configureFlags = '--disable-shared ';
 
-            if (process.platform === 'darwin') {
-                configureFlags += '--host i686-apple-darwin ';
-            }
+			if (process.platform === 'darwin') {
+				configureFlags += '--host i686-apple-darwin ';
+			}
 
-            if (process.platform === 'linux' && process.arch === 'x64') {
-                configureFlags += 'CFLAGS=\'-O3 -m64\' LDFLAGS=-m64';
-            } else {
-                configureFlags += 'CFLAGS=\'-O3 -m32\' LDFLAGS=-m32';
-            }
+			if (process.platform === 'linux' && process.arch === 'x64') {
+				configureFlags += 'CFLAGS=\'-O3 -m64\' LDFLAGS=-m64';
+			} else {
+				configureFlags += 'CFLAGS=\'-O3 -m32\' LDFLAGS=-m32';
+			}
 
-            var buildScript = './configure ' + configureFlags + ' &&' +
-                              'make install prefix=' + __dirname +
-                              '/libjpeg-turbo/ bindir=' + binDir +
-                              ' bin_PROGRAMS=jpegtran';
+			var buildScript = './configure ' + configureFlags + ' &&' +
+							  'make install prefix=' + __dirname +
+							  '/libjpeg-turbo/ bindir=' + binDir +
+							  ' bin_PROGRAMS=jpegtran';
 
-            exec(buildScript, { cwd: './libjpeg-turbo/' }, function (err) {
-                if (err) {
-                    return console.log(err);
-                }
+			exec(buildScript, { cwd: './libjpeg-turbo/' }, function (err) {
+				if (err) {
+					return console.log(err);
+				}
 
-                console.log('libjpeg-turbo rebuilt successfully'.green);
-            });
-        });
-    }
-}
+				console.log('libjpeg-turbo rebuilt successfully'.green);
+			});
+		});
+	}
+};
--- test/test-jpegtran-build.js
+++ test/test-jpegtran-build.js
@@ -6,13 +6,13 @@ var exec = require('child_process').exec;
 var binPath = require('../lib/jpegtran-bin.js').path;
 
 describe('JPEGTran rebuild', function () {
-    it('it should rebuild the jpegtran binaries', function (cb) {
-        this.timeout(false); // give this test time to build
-        var origCTime = fs.statSync(binPath).ctime;
-        exec('node build.js', {}, function (err) {
-            var actualCTime = fs.statSync(binPath).ctime;
-            assert(actualCTime !== origCTime);
-            cb(err);
-        }).path;
-    });
+	it('it should rebuild the jpegtran binaries', function (cb) {
+		this.timeout(false); // give this test time to build
+		var origCTime = fs.statSync(binPath).ctime;
+		exec('node build.js', {}, function (err) {
+			var actualCTime = fs.statSync(binPath).ctime;
+			assert(actualCTime !== origCTime);
+			cb(err);
+		}).path;
+	});
 });
