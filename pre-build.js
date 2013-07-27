a635cbcb6adcb13f5e9cd0697069ece097de4adf
Kevin Martensson <kevinmartensson@gmail.com>
2013-07-27 22:40:53 +0200

    Download binaries instead of including them

--- pre-build.js
+++ pre-build.js
@@ -1,16 +1,47 @@
 'use strict';
-var Mocha = require('mocha');
+
+var fs = require('fs');
+var path = require('path');
+var request = require('request');
 var colors = require('colors');
-var build = require('./build.js');
+var Mocha = require('mocha');
 var mocha = new Mocha({ui: 'bdd', reporter: 'min'});
+var build = require('./build.js');
 
-mocha.addFile('test/test-jpegtran-path.js');
+function runTest() {
+	mocha.addFile('test/test-jpegtran-path.js');
+	mocha.run(function (failures) {
+		if (failures > 0) {
+			console.log('pre-build test failed, compiling from source...'.red);
+			build();
+		} else {
+			console.log('pre-build test passed successfully, skipping build...'.green);
+		}
+	});
+}
 
-mocha.run(function (failures) {
-	if (failures > 0) {
-		console.log('pre-build test failed, compiling from source...'.red);
-		build();
+var binPath = require('./lib/jpegtran-bin').binPath;
+var binUrl = require('./lib/jpegtran-bin').binUrl;
+var dllPath = require('./lib/jpegtran-bin').dllPath;
+var dllUrl = require('./lib/jpegtran-bin').dllUrl;
+
+fs.exists(binPath, function (exists) {
+	if (exists) {
+		runTest();
 	} else {
-		console.log('pre-build test passed successfully, skipping build...'.green);
+		request.get(binUrl)
+			.pipe(fs.createWriteStream(binPath))
+			.on('close', function () {
+				fs.chmod(binPath, '0755');
+				if (process.platform === 'win32') {
+					request.get(dllUrl)
+						.pipe(fs.createWriteStream(dllPath))
+						.on('close', function () {
+							runTest();
+						});
+				} else {
+					runTest();
+				}
+			});
 	}
 });
