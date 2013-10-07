bcb6726681a00e45d108d676c479d072efa6553d
Kevin Martensson <kevinmartensson@gmail.com>
2013-10-07 10:46:00 +0200

    Use bin-wrapper to download, test and build jpegtran

--- test/test-build.js
+++ test/test-build.js
@@ -1,18 +1,39 @@
 /*global describe, it */
 'use strict';
-var fs = require('fs');
-var exec = require('child_process').exec;
-var assert = require('assert');
-var binPath = require('../lib/jpegtran.js').path;
 
-describe('JPEGTran rebuild', function () {
-	it('it should rebuild the jpegtran binaries', function (cb) {
-		this.timeout(false); // give this test time to build
-		var origCTime = fs.statSync(binPath).ctime;
-		exec('node build.js', {}, function (err) {
-			var actualCTime = fs.statSync(binPath).ctime;
+var assert = require('assert');
+var Bin = require('bin-wrapper');
+var fs = require('fs');
+var options = require('../lib/jpegtran').options;
+var path = require('path');
+
+describe('jpegtran.build()', function () {
+	it('should rebuild the jpegtran binaries', function (cb) {
+		this.timeout(false);
+		var configureFlags = '--disable-shared ';
+
+		if (process.platform === 'darwin') {
+			configureFlags += '--host i686-apple-darwin ';
+		}
+
+		if (process.platform === 'linux' && process.arch === 'x64') {
+			configureFlags += 'CFLAGS=\'-O3 -m64\' LDFLAGS=-m64';
+		} else {
+			configureFlags += 'CFLAGS=\'-O3 -m32\' LDFLAGS=-m32';
+		}
+
+		options.path = path.join(__dirname, '../tmp');
+		options.buildScript = './configure ' + configureFlags + ' &&' +
+							  ' make install bindir=' + path.join(__dirname, '../tmp') + ' bin_PROGRAMS=jpegtran';
+
+		var bin = new Bin(options);
+
+		bin.build(function () {
+			var origCTime = fs.statSync(bin.path).ctime;
+			var actualCTime = fs.statSync(bin.path).ctime;
+
 			assert(actualCTime !== origCTime);
-			cb(err);
-		}).path;
+			cb();
+		});
 	});
 });
--- test/test-path.js
+++ test/test-path.js
@@ -1,11 +1,12 @@
 /*global describe, it, after */
 'use strict';
+
+var assert = require('assert');
+var execFile = require('child_process').execFile;
 var fs = require('fs');
 var path = require('path');
-var exec = require('child_process').exec;
-var assert = require('assert');
 
-describe('jpegtran', function () {
+describe('jpegtran()', function () {
 	after(function () {
 		fs.unlinkSync('test/minified.jpg');
 	});
@@ -13,7 +14,7 @@ describe('jpegtran', function () {
 	it('should return path to jpegtran binary', function (cb) {
 		var binPath = require('../lib/jpegtran').path;
 
-		exec('"' + binPath + '" -v -', function (err, stdout, stderr) {
+		execFile(binPath, ['-v', '-'], function (err, stdout, stderr) {
 			assert(stderr.toString().indexOf('libjpeg-turbo') !== -1);
 			cb();
 		});
@@ -22,7 +23,7 @@ describe('jpegtran', function () {
 	it('should successfully proxy jpegtran', function (cb) {
 		var binPath = path.join(__dirname, '../bin/jpegtran.js');
 
-		exec('node "' + binPath + '" -v -', function (err, stdout, stderr) {
+		execFile('node', [binPath, '-v', '-'], function (err, stdout, stderr) {
 			assert(stderr.toString().indexOf('libjpeg-turbo') !== -1);
 			cb();
 		});
@@ -33,13 +34,14 @@ describe('jpegtran', function () {
 		var args = [
 			'-copy', 'none',
 			'-optimize',
-			'-outfile', '"' + path.join(__dirname, 'minified.jpg') + '"',
-			'"' + path.join(__dirname, 'fixtures', 'test.jpg') + '"'
+			'-outfile', path.join(__dirname, 'minified.jpg'),
+			path.join(__dirname, 'fixtures', 'test.jpg')
 		];
 
-		exec('node "' + binPath + '" ' + args.join(' '), function () {
+		execFile('node', [binPath].concat(args), function () {
 			var actual = fs.statSync('test/minified.jpg').size;
 			var original = fs.statSync('test/fixtures/test.jpg').size;
+
 			assert(actual < original);
 			cb();
 		});
