a835bd2d5a966e187c678586b83ef66722897d0a
Kevin Martensson <kevinmartensson@gmail.com>
2013-10-16 15:53:21 +0200

    Add quotes for cases where path contains spaces

--- test/test-build.js
+++ test/test-build.js
@@ -13,7 +13,7 @@ describe('jpegtran.build()', function () {
 		var bin = new Bin(options);
 
 		bin.path = path.join(__dirname, '../tmp', bin.bin);
-		bin.buildScript = './configure --disable-shared --bindir=' + path.join(__dirname, '../tmp') + ' && ' +
+		bin.buildScript = './configure --disable-shared --bindir="' + path.join(__dirname, '../tmp') + '" && ' +
 						  'make install';
 
 		bin.build(function (err) {
