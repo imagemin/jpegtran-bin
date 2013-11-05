fc0a69074b8c2e20c359e0ceca643c60fcf3f8f7
Kjetil Thuen <kjetil.thuen@nsd.uib.no>
2013-11-05 15:47:18 +0100

    Build libjpeg in user writeable folder.

--- test/test-build.js
+++ test/test-build.js
@@ -13,8 +13,7 @@ describe('jpegtran.build()', function () {
 		var bin = new Bin(options);
 
 		bin.path = path.join(__dirname, '../tmp', bin.bin);
-		bin.buildScript = './configure --disable-shared --bindir="' + path.join(__dirname, '../tmp') + '" && ' +
-						  'make install';
+		bin.buildScript = './configure --disable-shared --prefix="' + bin.path + '" && ' + 'make install';
 
 		bin.build(function (err) {
 			if (err) {
