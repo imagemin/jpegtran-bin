8068ddee07a8de656374cc24e2e8eacad764ff71
Kevin Martensson <kevinmartensson@gmail.com>
2014-02-14 16:49:14 +0100

    Rewrite

---
+++ index.js
@@ -0,0 +1,62 @@
+'use strict';
+
+var BinWrapper = require('bin-wrapper');
+var chalk = require('chalk');
+var fs = require('fs');
+var path = require('path');
+
+/**
+ * Initialize a new BinWrapper
+ */
+
+var bin = new BinWrapper({ bin: 'jpegtran', dest: path.join(__dirname, 'vendor') });
+var bs = './configure --disable-shared ' +
+		 '--prefix="' + bin.dest + '" && ' + 'make install';
+var args = [
+	'-copy', 'none',
+	'-optimize',
+	'-outfile', path.join(__dirname, 'test/tmp/test.jpg'),
+	path.join(__dirname, 'test/fixtures/test.jpg')
+];
+
+/**
+ * Only run check if binary doesn't already exist
+ */
+
+fs.exists(bin.path, function (exists) {
+	if (!exists) {
+		bin
+			.addUrl('https://raw.github.com/yeoman/node-jpegtran-bin/0.2.3/vendor/osx/jpegtran', 'darwin')
+			.addUrl('https://raw.github.com/yeoman/node-jpegtran-bin/0.2.3/vendor/linux/x86/jpegtran', 'linux', 'x86')
+			.addUrl('https://raw.github.com/yeoman/node-jpegtran-bin/0.2.3/vendor/linux/x64/jpegtran', 'linux', 'x64')
+			.addUrl('https://raw.github.com/yeoman/node-jpegtran-bin/0.2.3/vendor/freebsd/jpegtran', 'freebsd')
+			.addUrl('https://raw.github.com/yeoman/node-jpegtran-bin/0.2.3/vendor/sunos/x86/jpegtran', 'sunos', 'x86')
+			.addUrl('https://raw.github.com/yeoman/node-jpegtran-bin/0.2.3/vendor/sunos/x64/jpegtran', 'sunos', 'x64')
+			.addSource('http://downloads.sourceforge.net/project/libjpeg-turbo/1.3.0/libjpeg-turbo-1.3.0.tar.gz')
+			.check(args)
+			.on('error', function (err) {
+				console.log(chalk.red('✗ ' + err.message));
+			})
+			.on('fail', function () {
+				if (process.platform === 'win32') {
+					return console.log(chalk.red('✗ building is not supported on ' + process.platform));
+				}
+
+				console.log(chalk.red('✗ pre-build test failed, compiling from source...'));
+
+				this.build(bs);
+			})
+			.on('success', function () {
+				console.log(chalk.green('✓ pre-build test passed successfully'));
+			})
+			.on('finish', function () {
+				console.log(chalk.green('✓ jpegtran rebuilt successfully'));
+			});
+	}
+});
+
+/**
+ * Module exports
+ */
+
+module.exports.path = bin.path;
