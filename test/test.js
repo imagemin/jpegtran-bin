1de931d7c4e8c6c7a023d6716a58956be2be2687
Kevin Martensson <kevinmartensson@gmail.com>
2014-10-20 16:19:04 +0200

    Create and remove tmp directory before and after tests

--- test/test.js
+++ test/test.js
@@ -4,14 +4,12 @@ var binCheck = require('bin-check');
 var BinBuild = require('bin-build');
 var execFile = require('child_process').execFile;
 var fs = require('fs');
-var mkdir = require('mkdirp');
 var path = require('path');
-var rm = require('rimraf');
 var test = require('ava');
 var tmp = path.join(__dirname, 'tmp');
 
 test('rebuild the jpegtran binaries', function (t) {
-	t.plan(3);
+	t.plan(2);
 
 	var cfg = [
 		'./configure --disable-shared',
@@ -33,57 +31,41 @@ test('rebuild the jpegtran binaries', function (t) {
 
 		fs.exists(path.join(tmp, 'jpegtran'), function (exists) {
 			t.assert(exists);
-
-			rm(tmp, function (err) {
-				t.assert(!err);
-			});
 		});
 	});
 });
 
 test('return path to binary and verify that it is working', function (t) {
-	t.plan(3);
+	t.plan(2);
 
 	var args = [
-		'-outfile', path.join(tmp, 'test.jpg'),
+		'-outfile', path.join(tmp, 'test-path.jpg'),
 		path.join(__dirname, 'fixtures/test.jpg')
 	];
 
-	mkdir(tmp, function (err) {
+	binCheck(require('../').path, args, function (err, works) {
 		t.assert(!err);
-
-		binCheck(require('../').path, args, function (err, works) {
-			t.assert(!err);
-			t.assert(works);
-		});
+		t.assert(works);
 	});
 });
 
 test('minify a JPG', function (t) {
-	t.plan(6);
+	t.plan(4);
 
 	var args = [
 		'-outfile', path.join(tmp, 'test.jpg'),
 		path.join(__dirname, 'fixtures/test.jpg')
 	];
 
-	mkdir(tmp, function (err) {
+	execFile(require('../').path, args, function (err) {
 		t.assert(!err);
 
-		execFile(require('../').path, args, function (err) {
+		fs.stat(path.join(__dirname, 'fixtures/test.jpg'), function (err, a) {
 			t.assert(!err);
 
-			fs.stat(path.join(__dirname, 'fixtures/test.jpg'), function (err, a) {
+			fs.stat(path.join(tmp, 'test.jpg'), function (err, b) {
 				t.assert(!err);
-
-				fs.stat(path.join(tmp, 'test.jpg'), function (err, b) {
-					t.assert(!err);
-					t.assert(b.size < a.size);
-
-					rm(tmp, function (err) {
-						t.assert(!err);
-					});
-				});
+				t.assert(b.size < a.size);
 			});
 		});
 	});
