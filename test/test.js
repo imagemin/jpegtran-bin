f97dbc704e18762e07e806b10c74567d250bccd3
1000ch <shogo.sensui@gmail.com>
2017-03-25 20:09:43 +0900

    ES2015ify and require Node.js 4

--- test/test.js
+++ test/test.js
@@ -1,77 +1,47 @@
 'use strict';
-/* eslint-env mocha */
-var assert = require('assert');
-var execFile = require('child_process').execFile;
-var fs = require('fs');
-var path = require('path');
-var BinBuild = require('bin-build');
-var binCheck = require('bin-check');
-var compareSize = require('compare-size');
-var mkdirp = require('mkdirp');
-var rimraf = require('rimraf');
+const fs = require('fs');
+const path = require('path');
+const test = require('ava');
+const execa = require('execa');
+const tempy = require('tempy');
+const binCheck = require('bin-check');
+const BinBuild = require('bin-build');
+const compareSize = require('compare-size');
+const jpegtran = require('..');
 
-var tmp = path.join(__dirname, 'tmp');
-
-beforeEach(function () {
-	mkdirp.sync(tmp);
-});
-
-afterEach(function () {
-	rimraf.sync(tmp);
-});
-
-it('rebuild the jpegtran binaries', function (cb) {
-	var cfg = [
+test.cb('rebuild the jpegtran binaries', t => {
+	const tmp = tempy.directory();
+	const cfg = [
 		'./configure --disable-shared',
-		'--prefix="' + tmp + '" --bindir="' + tmp + '"'
+		`--prefix="${tmp}" --bindir="${tmp}"`
 	].join(' ');
 
 	new BinBuild()
 		.src('https://downloads.sourceforge.net/project/libjpeg-turbo/1.5.1/libjpeg-turbo-1.5.1.tar.gz')
 		.cmd(cfg)
 		.cmd('make install')
-		.run(function (err) {
-			if (err) {
-				cb(err);
-				return;
-			}
-
-			assert(fs.statSync(path.join(tmp, 'jpegtran')).isFile());
-			cb();
+		.run(err => {
+			t.ifError(err);
+			t.true(fs.existsSync(path.join(tmp, 'jpegtran')));
+			t.end();
 		});
 });
 
-it('return path to binary and verify that it is working', function () {
-	var args = [
-		'-outfile', path.join(tmp, 'test.jpg'),
-		path.join(__dirname, 'fixtures/test.jpg')
-	];
-
-	return binCheck(require('../'), args).then(assert);
+test('return path to binary and verify that it is working', async t => {
+	t.true(await binCheck(jpegtran, ['-version']));
 });
 
-it('minify a JPG', function (cb) {
-	var src = path.join(__dirname, 'fixtures/test.jpg');
-	var dest = path.join(tmp, 'test.jpg');
-	var args = [
+test('minify a JPG', async t => {
+	const tmp = tempy.directory();
+	const src = path.join(__dirname, 'fixtures/test.jpg');
+	const dest = path.join(tmp, 'test.jpg');
+	const args = [
 		'-outfile', dest,
 		src
 	];
 
-	execFile(require('../'), args, function (err) {
-		if (err) {
-			cb(err);
-			return;
-		}
+	await execa(jpegtran, args);
+	const res = await compareSize(src, dest);
 
-		compareSize(src, dest, function (err, res) {
-			if (err) {
-				cb(err);
-				return;
-			}
-
-			assert(res[dest] < res[src]);
-			cb();
-		});
-	});
+	t.true(res[dest] < res[src]);
 });
