aeb4b89760a76ffad533546610d913b3aebf4cca
1000ch <shogosensui@gmail.com>
2022-05-12 20:06:21 +0900

    Drop Node.js 12 support

--- test/test.js
+++ test/test.js
@@ -3,8 +3,8 @@ import path from 'node:path';
 import process from 'node:process';
 import {fileURLToPath} from 'node:url';
 import test from 'ava';
-import execa from 'execa';
-import tempy from 'tempy';
+import {execa} from 'execa';
+import {temporaryDirectory} from 'tempy';
 import binCheck from 'bin-check';
 import binBuild from 'bin-build';
 import compareSize from 'compare-size';
@@ -17,7 +17,7 @@ test('rebuild the jpegtran binaries', async t => {
 		return;
 	}
 
-	const temporary = tempy.directory();
+	const temporary = temporaryDirectory();
 	const cfg = [
 		'./configure --disable-shared',
 		`--prefix="${temporary}" --bindir="${temporary}"`,
@@ -37,7 +37,7 @@ test('return path to binary and verify that it is working', async t => {
 });
 
 test('minify a JPG', async t => {
-	const temporary = tempy.directory();
+	const temporary = temporaryDirectory();
 	const src = fileURLToPath(new URL('fixtures/test.jpg', import.meta.url));
 	const dest = path.join(temporary, 'test.jpg');
 	const args = [
